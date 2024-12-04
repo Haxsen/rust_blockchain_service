e0e11bb20d6db3afd64303745fa9db3f257bc021
Haxsen <hassaan.ali.lel@gmail.com>
2024-12-04 21:06:42 +0500

    [Refactor] Improved request models/schemas

--- src/models.rs
+++ src/models.rs
@@ -3,13 +3,19 @@ use utoipa::ToSchema;
 
 // Request and response structures for deposit, withdraw, and balance check
 #[derive(Deserialize, Serialize, ToSchema)]
-pub struct DepositRequest {
+pub struct DepositOrWithdraw {
     pub amount: String, // Amount to deposit (string representation)
 }
 
 #[derive(Deserialize, Serialize, ToSchema)]
-pub struct WithdrawRequest {
-    pub amount: String, // Amount to withdraw (string representation)
+pub struct DepositOrWithdrawWithWallet {
+    pub amount: String, // Amount to deposit (string representation)
+    pub privatekey: String, // Private key for client execution
+}
+
+#[derive(Deserialize, Serialize, ToSchema)]
+pub struct CheckWithWallet {
+    pub privatekey: String, // Private key for client execution
 }
 
 #[derive(Serialize)]
