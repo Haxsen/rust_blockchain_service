65f50a500c112fa15e2ed2f08b9003be49df6e32
Haxsen <hassaan.ali.lel@gmail.com>
2024-12-06 21:15:01 +0500

    Added new endpoints that work with priv key

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use actix_web::{web, App, HttpServer};
 use utoipa_swagger_ui::SwaggerUi;
 use dotenv::dotenv;
 use utoipa::OpenApi;
-use crate::routes::{deposit, withdraw, check_balance, root};
+use crate::routes::{deposit, withdraw, check_balance, root, deposit_with_wallet, withdraw_with_wallet, check_balance_with_wallet};
 use crate::api_docs::ApiDoc;
 
 mod routes;
@@ -23,8 +23,11 @@ async fn main() -> std::io::Result<()> {
             )
             .route("/", web::get().to(root))  // Root route returning a simple message
             .route("/deposit", web::post().to(deposit))  // Deposit route
+            .route("/deposit_with_wallet", web::post().to(deposit_with_wallet))
             .route("/withdraw", web::post().to(withdraw))  // Withdraw route
+            .route("/withdraw_with_wallet", web::post().to(withdraw_with_wallet))
             .route("/check_balance", web::post().to(check_balance))  // Check balance route
+            .route("/check_balance_with_wallet", web::post().to(check_balance_with_wallet))
     })
         .bind("127.0.0.1:8081")?  // Bind to localhost on port 8081
         .run()
