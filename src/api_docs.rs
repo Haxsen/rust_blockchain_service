49d568d94c42938baaee4359e30d0bf774d81fdf
Haxsen <hassaan.ali.lel@gmail.com>
2024-12-07 21:16:10 +0500

    Added swagger links / doc

--- src/api_docs.rs
+++ src/api_docs.rs
@@ -5,13 +5,17 @@ use utoipa::OpenApi;
 #[openapi(
     paths(
         crate::routes::deposit,
+        crate::routes::deposit_with_wallet,
         crate::routes::withdraw,
-        crate::routes::check_balance
+        crate::routes::withdraw_with_wallet,
+        crate::routes::check_balance,
+        crate::routes::check_balance_with_wallet
     ),
     // Request body schemas
     components(schemas(
-        crate::models::DepositRequest,
-        crate::models::WithdrawRequest
+        crate::models::DepositOrWithdraw,
+        crate::models::DepositOrWithdrawWithWallet,
+        crate::models::CheckWithWallet
     )),
     tags(
         (name = "Staking API", description = "Endpoints for user staking") // Group endpoints under 'Staking API' tag
